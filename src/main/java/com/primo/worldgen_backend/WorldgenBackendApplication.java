11ecb16bf13de84a00db809e67cc1211014916ec
AlePrimo <alejandrojuliancarullo@hotmail.com>
2025-10-26 19:27:50 -0300

    Comienzo del juego

---
+++ src/main/java/com/primo/worldgen_backend/WorldgenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.primo.worldgen_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldgenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldgenBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/primo/worldgen_backend/WorldgenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primo.worldgen_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldgenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
